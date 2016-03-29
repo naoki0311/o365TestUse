0af90c3b301530b472af54826353d28e742e084e
naoki0311 <naoki0311@gmail.com>
2016-03-29 15:32:18 +0900

    初コミット

---
+++ Office 365 Test Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Office_365_Test_Project.MvcApplication" Language="C#" %>
---
+++ Office 365 Test Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Office_365_Test_Project.Startup))]
+namespace Office_365_Test_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
